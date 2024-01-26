6afaa694a6ecf0257b8d57f44e4029d4fa177093
Eric Bode <eric.bode@foundries.io>
2024-01-26 13:36:39 +0100

    feat(plugin): added tmux navigator
    Signed-off-by: Eric Bode <eric.bode@foundries.io>

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,10 +1,12 @@
 return {
   {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
     "folke/todo-comments.nvim",
     lazy = false,
-    config = function()
-      require("todo-comments").setup()
-    end,
+    config = function() require("todo-comments").setup() end,
   },
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
